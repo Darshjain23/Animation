34dfa22053bcd009992a575f40904cdbae44fc51
Darsh <Darshjain2310@gmail.com>
2024-01-29 20:14:49 +0530

    29 jan

---
+++ zoom.js
@@ -0,0 +1,20 @@
+document.getElementsByClassName("zoom1")[0].onmouseover=function(){
+    document.getElementsByClassName("zoom1")[0].classList.add("zoom")
+}
+document.getElementsByClassName("zoom1")[0].onmouseleave=function(){
+    document.getElementsByClassName("zoom1")[0].classList.remove("zoom")
+}
+
+document.getElementsByClassName("zoom2")[0].onmouseover=function(){
+    document.getElementsByClassName("zoom2")[0].classList.add("zoom4")
+}
+document.getElementsByClassName("zoom2")[0].onmouseleave=function(){
+    document.getElementsByClassName("zoom2")[0].classList.remove("zoom4")
+}
+
+document.getElementsByClassName("zoom3")[0].onmouseover=function(){
+    document.getElementsByClassName("zoom3")[0].classList.add("zoom5")
+}
+document.getElementsByClassName("zoom3")[0].onmouseleave=function(){
+    document.getElementsByClassName("zoom3")[0].classList.remove("zoom5")
+}
